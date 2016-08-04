5cd1e5f8f001c1e1cd1a4cc1b5ba20d1fc95c2f1
Scott Meyer <scott.meyer@jrjrnetworks.com>
2016-08-04 13:52:52 -0500

    add initial plugin config controller and view

---
+++ src/Plugins/Nop.Plugin.ExternalAuth.OpenIdConnect/Controllers/ExternalAuthOpenIdConnectController.cs
@@ -0,0 +1,62 @@
+﻿using Nop.Core;
+using Nop.Plugin.ExternalAuth.OpenIdconnect.Models;
+using Nop.Services.Configuration;
+using Nop.Services.Security;
+using Nop.Services.Stores;
+using Nop.Web.Framework.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Nop.Plugin.ExternalAuth.OpenIdConnect.Controllers
+{
+    public class ExternalAuthOpenIdConnectController : BasePluginController
+    {
+        private ISettingService _settingService;
+        private IPermissionService _permissionService;
+        private IStoreService _storeService;
+        private IWorkContext _workContext;
+
+        public ExternalAuthOpenIdConnectController(
+            ISettingService settingService,
+            IPermissionService permissionService,
+            IStoreService storeService,
+            IWorkContext workContext
+            )
+        {
+            _settingService = settingService;
+            _permissionService = permissionService;
+            _storeService = storeService;
+            _workContext = workContext;
+
+        }
+        [AdminAuthorize]
+        [ChildActionOnly]
+        public ActionResult Configure()
+        {
+            if (!_permissionService.Authorize(StandardPermissionProvider.ManageExternalAuthenticationMethods))
+                return Content("Access denied");
+
+            //load settings for a chosen store scope
+            var storeScope = this.GetActiveStoreScopeConfiguration(_storeService, _workContext);
+            var oidcExternalAuthSettings = _settingService.LoadSetting<OpenIdConnectExternalAuthSettings>(storeScope);
+
+            var model = new ConfigurationModel();
+            model.ClientKeyIdentifier = oidcExternalAuthSettings.ClientId;
+            model.ClientSecret = oidcExternalAuthSettings.ClientSecret;
+
+            model.ActiveStoreScopeConfiguration = storeScope;
+            if (storeScope > 0)
+            {
+                model.ClientKeyIdentifier_OverrideForStore = _settingService.SettingExists(oidcExternalAuthSettings, x => x.ClientId, storeScope);
+                model.ClientSecret_OverrideForStore = _settingService.SettingExists(oidcExternalAuthSettings, x => x.ClientSecret, storeScope);
+            }
+
+            return View("~/Plugins/ExternalAuth.OpenIdConnect/Views/ExternalAuthOpenIdConnect/Configure.cshtml", model);
+        }
+
+    }
+}
---
+++ src/Plugins/Nop.Plugin.ExternalAuth.OpenIdConnect/Models/ConfigurationModel.cs
@@ -0,0 +1,18 @@
+﻿using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.ExternalAuth.OpenIdconnect.Models
+{
+    public class ConfigurationModel : BaseNopModel
+    {
+        public int ActiveStoreScopeConfiguration { get; set; }
+
+        [NopResourceDisplayName("Plugins.ExternalAuth.OpenIdConnect.ClientKeyIdentifier")]
+        public string ClientKeyIdentifier { get; set; }
+        public bool ClientKeyIdentifier_OverrideForStore { get; set; }
+
+        [NopResourceDisplayName("Plugins.ExternalAuth.OpenIdConnect.ClientSecret")]
+        public string ClientSecret { get; set; }
+        public bool ClientSecret_OverrideForStore { get; set; }
+    }
+}
