8fe37aa6c3ba54edda7c9f54559472e1a08e85a7
Chris Coulter <chris.coulter@jrjrnetworks.com>
2016-11-22 09:52:23 -0600

    remove PartyOrders plugin - no longer needed

--- src/Plugins/Nop.Plugin.Orders.Parties/Services/IPartyOrderService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Nop.Plugin.Orders.Parties.Domain;
-
-namespace Nop.Plugin.Orders.Parties.Services
-{
-    public interface IPartyOrderService
-    {
-        void Associate(PartyOrder partyOrder);
-    }
-}
